a8891fed3e59cb8623cd35417b8ccf044f517afa
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-04-28 23:58:39 +0600

    .

--- 3operator.py
+++ 3operator.py
@@ -10,3 +10,37 @@ print(sub)
 multi=a*b
 print(multi)
 
+divition=a/b
+print(divition)
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#modeular operator use
+print(a%b) #reminder
+print(a**b) #a^b
+
+#relational operator
+
+a=20
+b=10
+
+print(a==b)#False
+print(a!=b)#True
+print(a<b)#False
+print(a>b)#True
+print(a<=b)#False
+
+#aggignment operator
+
+num=10
+num=num+20
+print("Num = ",num)
+
+#logical operator
+
+print(not False)
+print(not True)
+
