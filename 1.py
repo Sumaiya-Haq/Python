c7a46f391a199d368ff14d933aa7d319e222d49d
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-04-29 23:05:18 +0600

    cooment out

--- 1.py
+++ 1.py
@@ -1,16 +1,22 @@
-#print("Hello Bangladesh")
-'''print your  
-self
-'''
-print("Hello Bangladesh")
-name ="Sumaiya Haq"
-age=22
+print("Hello Bangladesh")  # Prints "Hello Bangladesh" to the console
 
-print("My name is ",name)
-print("my age is ",age)
+print("print your")        # Prints "print your" to the console
+print("self")              # Prints "self" to the console
 
-age2=age
-print(age2)
+print("Hello Bangladesh")  # Prints "Hello Bangladesh" again
 
-print(type(name))
-print(type(age))
+name = "Sumaiya Haq"       # Assigns the string "Sumaiya Haq" to the variable 'name'
+
+age = 22                   # Assigns the integer 22 to the variable 'age'
+
+print("My name is ", name) # Prints "My name is " followed by the value of 'name'
+
+print("my age is ", age)   # Prints "my age is " followed by the value of 'age'
+
+age2 = age                 # Copies the value of 'age' into a new variable 'age2'
+
+print(age2)                # Prints the value of 'age2'
+
+print(type(name))          # Prints the data type of 'name' (which is a string)
+
+print(type(age))           # Prints the data type of 'age' (which is an integer)
