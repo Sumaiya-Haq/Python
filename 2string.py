55d99d552314902d5a2ab989c80b2fc3d271c2d5
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-04-30 22:38:36 +0600

    .

--- 2string.py
+++ 2string.py
@@ -4,3 +4,19 @@ name = "Sumaiya haq"
 # Assigning the same string using single quotes
 name1 = 'Sumaiya haq'
 
+# Assigning the same string using triple quotes (also used for multi-line strings)
+name2 = '''Sumaiya haq'''
+
+# Printing the value stored in variable 'name'
+print(name)
+
+# Printing the value stored in variable 'name1'
+print(name1)
+
+# Printing the value stored in variable 'name2'
+print(name2)
+
+# Assigning an integer value to variable 'age'
+age = 22
+
+
