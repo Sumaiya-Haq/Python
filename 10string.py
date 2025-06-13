1c2d00514be80d610b80f830b966fd3d08ecf094
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-06-13 10:55:14 +0600

    different type string print

---
+++ 10string.py
@@ -0,0 +1,19 @@
+#String print type
+
+str1="hi Sumaiya"
+str2='hi Sumaiya'
+str3='''hi Sumaiy'''
+#different cotation but print same string.
+print(str1)
+print(str2)
+print(str3)
+
+# two line print in same line.
+
+str4="Previous i show my simple knowledge in python. Now i clear string in python ."
+print(str4)
+
+# two line print in another line.
+
+str5="hi dear.\tI'm a coder in cse.\nRecently i learn some unice coding ."
+print(str5)
