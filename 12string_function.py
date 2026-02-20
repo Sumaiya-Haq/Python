267992e3e04b443f32d8d0741d7f8a794616ae40
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-02-20 20:45:10 +0600

    replace function

--- 12string_function.py
+++ 12string_function.py
@@ -9,3 +9,9 @@ str ="most beautiful hearted person in the world"
 print(str)
 print(str.capitalize()) #only first letter of the string will be capitalized
 str= print(str.capitalize())
+
+#using replace function
+
+str="in future i want to be a AI Engineer"
+print(str.replace("I","e")) #replace all the I with e
+print(str.replace("AI","ML")) #replace AI with ML
