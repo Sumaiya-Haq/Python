c8caea55d3fc245810da3886f63a22cf210e864d
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-02-20 20:38:03 +0600

    String Function

--- 12endswith.py
+++
@@ -1,3 +0,0 @@
-str ="I am studing BCSE at IUBAT University"
-print(str.endswith("sity")) #True
-print(str.endswith("t")) #False
---
+++ 12string_function.py
@@ -0,0 +1,11 @@
+str ="I am studing BCSE at IUBAT University"
+print(str.endswith("sity")) #True
+print(str[0:37])
+print(str.endswith("t")) #False
+
+#using capatilize functon
+
+str ="most beautiful hearted person in the world"
+print(str)
+print(str.capitalize()) #only first letter of the string will be capitalized
+str= print(str.capitalize())
