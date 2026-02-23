f9d0fb8a77a82ee0d62fcdee8739e18f4e5249d6
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-02-23 23:32:33 +0600

    nested loop

--- 13conditional_statement.py
+++ 13conditional_statement.py
@@ -5,3 +5,14 @@ if(age >18):
 elif(age == 18):
     print("You are eligible for both driving license and vote")
 else:    print("You are not eligible for driving license and vote")
+
+
+#NESTING IF STATEMENT
+age= 99
+if(age > 18):
+    if(age>=90):
+        print("You are too old to drive")
+    else:
+        print("You are eligible for driving license")
+else:
+    print("You are not eligible for driving license")
