e06aadfa7ac1288d2a70c8579e5634a5fe6bdd2d
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-02-23 23:09:14 +0600

    APPLY CONDITION

--- 13conditional_statement.py
+++ 13conditional_statement.py
@@ -1,5 +1,7 @@
-age= 20
-if(age >=18):
+age= input("Enter your age: ")
+age= int(age) #convert the input to integer
+if(age >18):
   print("You are eligible for driving license")
-elif(age < 18):
-    print("You are not eligible for driving license")
+elif(age == 18):
+    print("You are eligible for both driving license and vote")
+else:    print("You are not eligible for driving license and vote")
