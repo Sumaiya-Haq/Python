e3088929b23ae1d70da2b7207b456c4274d261ea
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-02-23 23:39:09 +0600

    check multiplication

--- 13conditional_statement.py
+++ 13conditional_statement.py
@@ -19,6 +19,7 @@ else:
 
 
     #WAP to check if a number entered by the user is odd or even.
+
 number= int(input("Enter a number: "))
 if(number%2==0):
     print("The number is even")
@@ -27,3 +28,22 @@ else:
   
 
   #WAP to find the greatest of 3 numbers entered by the user.
+
+number1= int(input("Enter 1st number: "))
+number2= int(input("Enter 2nd number: ")) 
+number3= int(input("Enter 3rd number: "))
+if(number1>number2 and number1>number3):
+    print("The greatest number is", number1)
+elif(number2>number1 and number2>number3):
+    print("The greatest number is", number2)
+else:
+    print("The greatest number is", number3)
+
+  
+  #WAP to check if a number is a multiple of 7 or not.
+  
+number= int(input("Enter a number: "))
+if(number%7==0):
+    print("The number is a multiple of 7")
+else:  
+    print("The number is not a multiple of 7")
