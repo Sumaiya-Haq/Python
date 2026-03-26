c80de7c656b341085e8dcc433a4f4fc082f756ee
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-03-26 20:22:53 +0600

    identify

--- List and tuples/1list.py
+++ List and tuples/1list.py
@@ -1,3 +1,13 @@
 #A built-in data type that stores set of values
 #It can store elements of different types (integer, float, string, etc.)
 
+#Creating a list
+student=[21,23,25,67,76,90]
+print(student)
+print(type(student))
+print("student[1]:",student[0])
+print("student[2]:",student[1])
+print("student[3]:",student[2])
+print("student[4]:",student[3])
+print("student[5]:",student[4])
+print("student[6]:",student[5])
