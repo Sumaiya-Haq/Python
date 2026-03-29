5084f95cc5cac11d18fa689292e133294ef57fa1
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-03-29 21:46:53 +0600

    add negative clicing

--- List and tuples/3list_slycing.py
+++ List and tuples/3list_slycing.py
@@ -1,3 +1,10 @@
 #semiler to string slicing we can slice list as well
 
-student = ["sumaiya", 3.67, "tongi", ""]
+student = ["sumaiya", 3.67, "tongi"]
+print(student[2]) #tongi
+
+marks= [34,56,78,24,69,44]
+print(marks[1:3]) #[56, 78] it will not include the last index
+print(marks[1:])
+
+print(marks[-3:-2]) #69
