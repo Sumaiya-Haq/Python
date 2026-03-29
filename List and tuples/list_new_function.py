5a6e00f7157e482def3d1f2b354e6ca878903949
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-03-29 22:02:18 +0600

    sort function

--- List and tuples/list_new_function.py
+++ List and tuples/list_new_function.py
@@ -3,3 +3,16 @@
 list= [3,4,5,6,7,8,9]
 list.append(10)
 print(list)
+
+#list.sort( )  #sorts in ascending order 
+
+list.sort() #automatically sort the list in ascending order
+print(list)
+list.sort(reverse=True) #sorts in descending order
+print(list)
+
+char=['a','b','c','d','e','g','f']
+char.sort()
+print(char)
+char.sort(reverse=True)
+print(char)
