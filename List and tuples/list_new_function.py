dd7353cfb52d5ff0d4dc052b2f25630d363d2c51
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-03-29 21:53:07 +0600

    added element in our list

---
+++ List and tuples/list_new_function.py
@@ -0,0 +1,5 @@
+#Append function is used to add an element at the end of the list. It takes only one argument which is the element to be added. The syntax for append function is as follows:
+
+list= [3,4,5,6,7,8,9]
+list.append(10)
+print(list)
