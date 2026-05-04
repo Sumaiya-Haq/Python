f4d45876367684e860f67c062d75c6be36f6c3dd
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2026-05-04 22:25:07 +0600

    solve question

---
+++ List and tuples/tuples&list_questrionaa-solve.py
@@ -0,0 +1,27 @@
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+
+
+movie = []
+mov1= input("Enter your 1st movie name= ")
+mov2= input("Enter your 2nd movie name= ")
+mov3= input("Enter your 3rd movie name= ")
+
+movie.append(mov1)
+movie.append(mov2)
+movie.append(mov3)
+print("Your favorite movies are: ", movie)
+
+
+
+
+#WAP to check if a list contains a palindrome of elements. (Hint: use copy( ) method)
+list = [1,2,3]
+list2 = [1,2,3]
+
+copy_list = list.copy()
+copy_list.reverse()
+
+if(copy_list == list):
+    print("palindrom")
+else:
+    print("not palindrom")
